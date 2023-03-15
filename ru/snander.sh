3927db5005d6e3e3fa064b50735087f96eca8533
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2023-03-15 14:37:09 +0300

    Update snander.sh
    Adding the information about NOR and NAND chips

--- ru/snander.sh
+++ ru/snander.sh
@@ -10,11 +10,12 @@ epromtype=$(zenity --height=320 --width=320 --list --radiolist --text \
  FALSE "24Cxx - Two-Wire Serial EEPROM"\
  FALSE "93Cxx - MICROWIRE Serial EEPROM"\
  FALSE "25Cxx - NAND FLASH EEPROM"\
- FALSE "25Qxx - NOR FLASH EEPROM"\ )
+ FALSE "25Qxx - NOR FLASH EEPROM")
  if [ -n "$epromtype" ]
  then
      if [[ "$epromtype" == "24Cxx - Two-Wire Serial EEPROM" ]]
      then
+     echo "24Cxx selected"
      eeprom_model=$(zenity --height=330 --width=320 --list --radiolist --text \
 "24Cxx - Two-Wire Serial EEPROM:" --column="Set" --column="Модель микросхемы"\
  TRUE "24c01"\
@@ -29,6 +30,7 @@ epromtype=$(zenity --height=320 --width=320 --list --radiolist --text \
      fi
      if [[ "$epromtype" == "93Cxx - MICROWIRE Serial EEPROM" ]]
      then
+     echo "93Cxx selected"
      eeprom_model=$(zenity --height=320 --width=320 --list --radiolist --text \
 "93Cxx - Microwire EEPROM:" --column="Set" --column="Модель микросхемы"\
  TRUE "93c06"\
@@ -39,6 +41,45 @@ epromtype=$(zenity --height=320 --width=320 --list --radiolist --text \
  FALSE "93c76"\
  FALSE "93c86"\
  FALSE "93c96"\ )
+     fi
+     if [ "$epromtype" == "25Cxx - NAND FLASH EEPROM" ] || [ "$epromtype" == "25Qxx - NOR FLASH EEPROM" ]
+     then
+        echo "NOR or NAND selected"
+        manufacture='Производитель: '
+        info=$(SNANDer -i)
+        man_code=$(echo "$info" | grep 'spi device id:')
+        if [[ $man_code == *"spi device id:"* ]] 
+          then
+             if [[ $man_code == *"id: 01"* ]]; then man_name="SPANSION"; fi
+             if [[ $man_code == *"id: 0b"* ]]; then man_name="XTX"; fi
+             if [[ $man_code == *"id: 1c"* ]]; then man_name="Eon"; fi
+             if [[ $man_code == *"id: 1f"* ]]; then man_name="Atmel"; fi
+             if [[ $man_code == *"id: 20"* ]]; then man_name="Micron"; fi
+             if [[ $man_code == *"id: 5e"* ]]; then man_name="Zbit"; fi
+             if [[ $man_code == *"id: 68"* ]]; then man_name="Boya"; fi
+             if [[ $man_code == *"id: 7f"* ]]; then man_name="ISSI"; fi
+             if [[ $man_code == *"id: 85"* ]]; then man_name="PUYA"; fi
+             if [[ $man_code == *"id: 9d"* ]]; then man_name="ISSI"; fi
+             if [[ $man_code == *"id: a1"* ]]; then man_name="Fudan"; fi
+             if [[ $man_code == *"id: ba"* ]]; then man_name="Zetta"; fi
+             if [[ $man_code == *"id: c2"* ]]; then man_name="MXIC (Macronix)"; fi
+             if [[ $man_code == *"id: c8"* ]]; then man_name="GigaDevice"; fi
+             if [[ $man_code == *"id: e0"* ]]; then man_name="PARAGON"; fi
+             if [[ $man_code == *"id: ef"* ]]; then man_name="Winbond"; fi   
+             if [[ $man_code == *"id: f8"* ]]; then man_name="Fudan"; fi   
+          else  manufacture="Производитель не найден."
+       fi;
+       manufacture="${manufacture}<b>${man_name}</b>"
+       info=$(echo "$info" | grep 'Flash:')
+       rufirst="Найдена микросхема "
+       rusecond="Объем: "
+       ruinfo=${info/'Detected '/"\n$rufirst"}
+       ruinfo=${ruinfo/'Flash Size:'/"\n$rusecond"}
+       ruinfo=${ruinfo/'[93m'/"<b>"}
+       ruinfo=${ruinfo/'[93m'/"<b>"}
+       ruinfo=${ruinfo/'[0m'/"</b>"}
+       ruinfo=${ruinfo/'[0m'/"</b>"}
+       ruinfo="${ruinfo} \n${manufacture}"   
      fi
      action_type=$(zenity --height=340 --width=320 --list --radiolist --text \
 "Выберите действие:" --column="Set" --column="Действие"\
@@ -62,9 +103,9 @@ epromtype=$(zenity --height=320 --width=320 --list --radiolist --text \
                                     --auto-kill --auto-close \
                                     --percentage=10)
         else
-        SNANDer -r $filepath/$filename | tee >(zenity --width=200 --height=100 \
+        SNANDer -r $filepath/$filename | tee >(zenity --width=320 --height=100 \
   				    --title="Считывание" --progress \
-			            --pulsate --text="Подождите, процесс выполняется..." \
+			            --pulsate --text="$ruinfo\n\nПодождите, процесс выполняется..." \
                                     --auto-kill --auto-close \
                                     --percentage=10)
         fi
@@ -82,9 +123,9 @@ epromtype=$(zenity --height=320 --width=320 --list --radiolist --text \
                                     --auto-kill --auto-close \
                                     --percentage=10)
         else
-        SNANDer -w $filename | tee >(zenity --width=200 --height=100 \
+        SNANDer -w $filename | tee >(zenity --width=320 --height=100 \
   				    --title="Запись" --progress \
-			            --pulsate --text="Подождите, процесс выполняется..." \
+			            --pulsate --text="$ruinfo\n\nПодождите, процесс выполняется..." \
                                     --auto-kill --auto-close \
                                     --percentage=10)
         fi
@@ -99,9 +140,9 @@ epromtype=$(zenity --height=320 --width=320 --list --radiolist --text \
                                     --auto-kill --auto-close \
                                     --percentage=10)
         else
-        SNANDer -e | tee >(zenity --width=200 --height=100 \
+        SNANDer -e | tee >(zenity --width=320 --height=100 \
   				    --title="Стирание" --progress \
-			            --pulsate --text="Подождите, процесс выполняется..." \
+			            --pulsate --text="$ruinfo\n\nПодождите, процесс выполняется..." \
                                     --auto-kill --auto-close \
                                     --percentage=10)
         fi
